e6b2b1360974746545eb866cf1f286360dd4df63
Neameh Karineh <neamehkh92@gmail.com>
2022-08-11 05:50:17 +0300

    Implement The PlansTable

---
+++ src/PlannerApp/Components/Plans/PlansTable.razor.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using PlannerApp;
+using PlannerApp.Shared;
+using MudBlazor;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using PlannerApp.Shared.Components;
+using Blazored.FluentValidation;
+using PlannerApp.Client.Services.Interfaces;
+using PlannerApp.Shared.Models;
+
+namespace PlannerApp.Components
+{
+    public partial class PlansTable
+    {
+        [Inject]
+        public IPlanService PlanService { get; set; }
+        [Parameter]
+        public EventCallback<PlanSummary> OnViewClicked { get; set; }
+        [Parameter]
+        public EventCallback<PlanSummary> OnEditClicked { get; set; }
+        [Parameter]
+        public EventCallback<PlanSummary> OnDeleteClicked { get; set; }
+
+        private string _query = string.Empty;
+        private MudTable<PlanSummary> _table; // what this ? 
+
+        private async Task<TableData<PlanSummary>> ServerReloadAsync(TableState state)
+        {
+            var result = await PlanService.GetPlanSync(_query,state.Page,state.PageSize);
+            return new TableData<PlanSummary>()
+            {
+                Items = result.Value.Records,
+                TotalItems = result.Value.ItemCount
+            };
+        }
+        private void OnSearch(string query)
+        {
+            _query = query;
+            _table.ReloadServerData();
+        }
+    }
+}
